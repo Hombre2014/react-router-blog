a673803c018d1eafa12870017bbc7af5c8d5594e
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-06-07 18:54:42 +0200

    All functionality have been implemented

--- src/PostPage.js
+++ src/PostPage.js
@@ -1,7 +1,28 @@
-const PostPage = () => {
+import { useParams, Link } from 'react-router-dom';
+
+const PostPage = ({ posts, handleDelete }) => {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
   return (
-    <main>
-      <h1>Post Page</h1>
+    <main className="PostPage">
+      <article className="post">
+        {post &&
+          <>
+            <h2>{post.title}</h2>
+            <p className="postDate">{post.datetime}</p>
+            <p className="postBody">{post.body}</p>
+            <button onClick={() => handleDelete(post.id)}>Delete</button>
+            <Link to="/">Back to posts</Link>
+          </>
+        }
+        {!post &&
+          <>
+            <h2>Post not found</h2>
+            <p>Well, that's disappointing.</p>
+            <Link to="/">Back to posts</Link>
+          </>
+        }
+      </article>
     </main>
   )
 }
